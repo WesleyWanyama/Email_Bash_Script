5a3a71ccabcaf95efb9389eb32c342c20c637fdc
Wesley Wanyama <83776940+WesleyWanyama@users.noreply.github.com>
2024-02-21 11:14:26 +0300

    Update email.sh to accept user input

--- email.sh
+++ email.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ssmtp user@domain.com < msg.txt
+echo Enter Recepient Address
+read Recepient
+echo Enter email content
+read body
+
+#Send the email using ssmtp
+#echo -e "Subject:$subject\n$body" | ssmtp "$Recepient"
+echo "$body" | ssmtp $Recepient
